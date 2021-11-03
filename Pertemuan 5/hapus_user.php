4f8f9ad8a5ff50b3940b977fb47d761c99869631
DedsecHAP <49938387+DedsecHAP@users.noreply.github.com>
2021-11-03 15:44:23 +0700

    Delete Pertemuan 5 directory

--- Pertemuan 5/hapus_user.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "koneksi.php";
-    $sql="DELETE FROM users WHERE UserID= '$_GET[id]'";
-    mysqli_query($con, $sql);
-    mysqli_close($con);
-    header('location:tampil_user.php');
-?>

920488b875067af39f6f7564b1e8f39f25b85bac
Elucidator-PC <hamasadiyanprasetyo@gmail.com>
2021-12-23 10:53:01 +0700

    (Postest) Syntax Explanation and Search Service

--- Pertemuan 10/akses_mhs.php
+++ Pertemuan 10/akses_mhs.php
@@ -1,20 +1,52 @@
 <?php 
-    $url = "http://localhost/pwd/Pertemuan%2010/getdatamhs.php";
-    $client = curl_init($url);
-    curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
-    $respone = curl_exec($client);
-    $result = json_decode($respone);
-    foreach ($result as $r) {
-        echo "<p>";
-            echo "NIM       : " .$r->Nim. "</br>";
-            echo "Nama      : " .$r->Nama. "</br>";
-            echo "Jenis Kel : " .gender($r->Jkel). "</br>";
-            echo "Alamat    : " .$r->Alamat. "</br>";
-            echo "Tgl Lahir : " .$r->Tgl_Lahr. "</br>";
-            echo "Prodi     : " .$r->Prodi. "</br>";
-        echo "</p>";
+    $cari = "";
+    if(isset($_POST['cari'])) {
+        // Menangkap data cari yang dikirim dengan method GET
+        $cari = $_POST['cari'];
+        // Menampilkan value dari variable cari
     }
+    echo "<h3>Form Pencarian Dengan Data MAHASISWA</h3>
+    <form action='akses_mhs.php' method='POST'>
+        <label>Cari :</label>
+        <input type='text' name='cari' value='$cari' placeholder='Masukan Nama'>
+        <input type='submit' value='Cari'>
+    </form>";
+    // kondisi untuk mengecek kiriman dengan method GET
 
+    // Menginisialisasikan link ke dalam variable url
+    $url = "http://localhost/pwd/Pertemuan%2010/getdatamhs.php?nim=$cari";
+
+    // Menginisialisasikan request http ke link pada variabel $url
+    $client = curl_init($url);
+
+    // Memberikan tambahan options untuk proses request
+    curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+
+    // Mengeksekusi perintah untuk request htpp sesuai options yang sudah ditambahkan
+    $respone = curl_exec($client);
+    
+    // Mengkonversi objek JSON menjadi objek PHP
+    $result = json_decode($respone);
+
+    // Perulangan untuk menampilkan ke halaman web site
+    // setiap data yang telah direqeust, sesuai dengan atribut keynya
+    if (empty($result)) {
+        echo "DATA KOSONG";
+    } else {
+        foreach ($result as $r) {
+            echo "<p>";
+                echo "NIM       : " .$r->Nim. "</br>";
+                echo "Nama      : " .$r->Nama. "</br>";
+                echo "Jenis Kel : " .gender($r->Jkel). "</br>";
+                echo "Alamat    : " .$r->Alamat. "</br>";
+                echo "Tgl Lahir : " .$r->Tgl_Lahr. "</br>";
+                echo "Prodi     : " .$r->Prodi. "</br>";
+            echo "</p>";
+        }
+    }
+?>
+
+<?php 
     function gender($gender) {
         if ($gender == 'L')
             return "Laki-Laki";
--- Pertemuan 10/getdatamhs.php
+++ Pertemuan 10/getdatamhs.php
@@ -1,14 +1,32 @@
 <?php 
+    // Memasukan File Koneksi.php
     require_once("koneksi.php");
-    $sql = "SELECT * FROM Mahasiswa";
+    // Menginisialisasi variabel $sql dengan query mysql
+    if (!empty($_GET['nim'])) {
+        $nim = $_GET['nim'];
+        $sql = "SELECT * FROM Mahasiswa WHERE Nim = '$nim'";
+    } else
+        $sql = "SELECT * FROM Mahasiswa";
+
+    // mengeksekusi perintah sql sesuai query pada 
+    // variable $sql, lalu memasukan return valuenya
+    // ke dalam variable $query
     $query = mysqli_query($con, $sql);
 
+    // Peruangan untuk memindahkan data dari database ke 
+    // array 1 dimensi.
     while ($row = mysqli_fetch_assoc($query)) {
         $data[] = $row;
     }
 
+    // Menginisialisasikan tipe konten JSON untuk
+    // header pada html.
     header('content-type: application/json');
+
+    // Menampilkan data ke halaman website dengan
+    // format JSON.
     echo json_encode($data);
 
     mysqli_close($con);
 ?>
+
